a44d971f31685a00fc2f5f8c481146d10f0b2b57
Anton Webb <anton25360@gmail.com>
2021-09-30 13:06:34 +0100

    Add firebase

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Anton Webb's Portfolio
 
 This is the repository for my website at https://antonwebb.me. Created in React.
+
+Use `npm install` and `npm start` to launch local version
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3BWyMb8vrqFAdjiTTZAabWGXxuGICgok",
+  authDomain: "antonwebb-site.firebaseapp.com",
+  projectId: "antonwebb-site",
+  storageBucket: "antonwebb-site.appspot.com",
+  messagingSenderId: "646396489891",
+  appId: "1:646396489891:web:cd812084b0ed69f5bd4f16",
+  measurementId: "G-6V6R5E7MP1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
